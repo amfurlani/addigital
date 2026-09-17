b744d2c93389278e606da651a6b5769a65d4e068
amfurlani <a.furlani@gmail.com>
2026-09-17 16:22:57 -0300

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -11,7 +11,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
-      url: `${baseUrl}/atuacao`,
+      url: `${baseUrl}/areas-de-atuacao`,
       changeFrequency: 'monthly',
       priority: 0.9,
     },
