e1bf62d871aeb39cf48ff6f77267dfd5dcac532a
amfurlani <a.furlani@gmail.com>
2026-09-17 17:40:16 -0300

    Update site.ts

--- lib/site.ts
+++ lib/site.ts
@@ -1,17 +1,47 @@
 export const site = {
   name: 'AD | Advocacia Digital',
+  displayName: 'AD - Advocacia Digital',
   shortName: 'AD Digital',
+
   legalName: 'FURLANI, Adilson Marques - Advogado',
+  responsibleName: 'FURLANI, Adilson Marques',
+
   tagline: 'Advocacia técnica, estratégica e próxima.',
-  description: 'Escritório de advocacia em São Paulo com atuação consultiva e contenciosa em áreas civís e empresariais.',
+
+  description:
+    'Advocacia em São Paulo e Grande São Paulo, com atuação consultiva e contenciosa para empresas e pessoas.',
+
   city: 'São Paulo · SP',
-  oab: 'OAB/SP 53.8189',
-  email: 'contato@addigital.adv.br',
-  phone: '+55 11 99342.0370',
-  address: 'Av. Gen. Mac Arthur, 1195 · Jaguaré · São Paulo · SP',
-  linkedin: '#',
-  instagram: '#',
-  youtube: '#',
+
+  oab: 'OAB/SP 538.189',
+
+  email: 'a.furlani+adv@gmail.com',
+
+  phone: '(11) 99342-0370',
+  phoneInternational: '+5511993420370',
+  whatsapp: '5511993420370',
+
+  address:
+    'Av. Gen. Mac Arthur, 1195 · Jaguaré · São Paulo/SP · CEP 05338-001',
+
+  streetAddress: 'Av. Gen. Mac Arthur, 1195',
+  neighborhood: 'Jaguaré',
+  addressLocality: 'São Paulo',
+  addressRegion: 'SP',
+  postalCode: '05338-001',
+  addressCountry: 'BR',
+
+  serviceArea:
+    'São Paulo e Grande São Paulo',
+
+  appointment:
+    'Atendimento somente com hora marcada. O primeiro atendimento é realizado online. Atendimentos posteriores poderão ser presenciais quando pertinentes.',
+
+  linkedin:
+    'https://www.linkedin.com/company/furlanipinheiros',
+
+  instagram: '',
+  youtube: '',
 };
 
 export const nav = [
